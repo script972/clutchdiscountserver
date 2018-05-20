57d46a13cad0d48b033c734c728495bd29fd59ea
script972 <script972@gmail.com>
2018-05-20 15:51:45 +0300

    heroku deploy

--- src/main/java/com/script972/dao/CardItemDAO.java
+++ src/main/java/com/script972/dao/CardItemDAO.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.List;
 
+
 @Transactional
 @Repository
 public class CardItemDAO implements CardRepository {
